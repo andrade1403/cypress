7e98282a5406f857e56e548f934dc8130b1d27bd
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-07 21:03:43 -0500

    Refactor a la clase principal pages.

--- e2e/crear_post.cy.js
+++ e2e/crear_post.cy.js
@@ -170,4 +170,77 @@ describe('Escenarios E2E para Ghost', function () {
         //Then debería ver el post publicado en la lista de posts
         PostPage.lastPostCreated(tituloEditado, 'notClick');
     });
+
+    it('E0004 - Editar el titulo y contenido de un post previamente creado', function () {
+        //Given que estoy en la pagina del login del Admin
+        cy.visit('http://localhost:2368/ghost');
+
+        //When escriba mis credenciales
+        LogIn.logIn('d.andrades@uniandes.edu.co', 'ArpolisVI204*');
+
+        //And le de click en el boton de Sign In
+        LogIn.logInButton();
+
+        //Then debería ver la página principal del administrador y su titulo
+        PrincipalPage.getTitle().should('have.text', 'MISW4103');
+
+        //When le de click en la sección de Posts
+        PrincipalPage.clickPosts();
+
+        //Then el administrador debería ver la página de listado de posts
+        PostPage.getTitleSection().should('include.text', 'Posts');
+
+        //When le de click en el boton New Post
+        PostPage.clickNewPost();
+
+        //Then el administrador debería ver la página de creación de post
+        PostPage.creationPostPage().should('have.value', '');
+
+        //When escriba el titulo del post
+        let titulo = faker.lorem.word();
+        PostPage.writeTitle(titulo);
+
+        //And el contenido del post
+        let contenido = faker.lorem.words();
+        PostPage.writeContent(contenido);
+
+        //And le de click en el boton de Publish
+        PostPage.publishPostButton();
+        cy.wait(1000);
+
+        //And le de click en el boton Continue, final review
+        PostPage.continueButton();
+       
+        //And le de click en el boton Publish post, right now
+        PostPage.publishPostButtonFinal();
+
+        //And cierre el modal de confirmación de publicación
+        PostPage.closePublishModal();
+
+        //Then debería ver el post publicado en la lista de posts
+       PostPage.lastPostCreated(titulo, 'notClick');
+
+        //When le de click en el post creado
+        PostPage.lastPostCreated(titulo, 'click');
+        
+        //Then edito el titulo del post
+        let tituloEditado = faker.lorem.word();
+        PostPage.writeTitle(tituloEditado);
+
+        //Then edito el contenido del post
+        let contenidoEditado = faker.lorem.words();
+        PostPage.writeContent(contenidoEditado);
+
+        //And le de click en el boton de update
+        PostPage.updatePostButton();
+
+        //And le de click en el boton de devolverme a la lista de posts
+        PostPage.clickBackToPosts();
+
+        //Then debería ver el post publicado en la lista de posts
+        PostPage.lastPostCreated(tituloEditado, 'notClick');
+
+        //When le de click en viewsite
+        PrincipalPage.clickSitePage();
+    });
 });
--- pages/principalPage.js
+++ pages/principalPage.js
@@ -3,14 +3,18 @@ export class PrincipalPage {
         return cy.get('.gh-nav-menu-details-sitetitle');
     }
     static clickPosts() {
-        return cy.get('#ember19').click({ force: true });
+        return cy.get('[data-test-nav="posts"]').click({ force: true });
     }
 
     static clickPages() {
-        return cy.get('#ember28').click({ force: true });
+        return cy.get('[data-test-nav="pages"]').click({ force: true });
     }
 
     static clickTags() {
-        return cy.get('#ember29').click({ force: true });
+        return cy.get('[data-test-nav="tags"]').click({ force: true });
+    }
+
+    static clickSitePage() {
+        return cy.get('[data-test-nav="site"]').click({ force: true });
     }
 }
